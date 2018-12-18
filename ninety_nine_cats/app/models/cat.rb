806543be72ebe24b84b87e65e90d77c28a6ac9ab
joedspin <jtdespinosa@gmail.com>
2018-12-18 15:59:36 -0500

    Flesh out the Cat class

---
+++ ninety_nine_cats/app/models/cat.rb
@@ -0,0 +1,30 @@
+# == Schema Information
+#
+# Table name: cats
+#
+#  id          :bigint(8)        not null, primary key
+#  birth_date  :date             not null
+#  color       :string           not null
+#  name        :string           not null
+#  sex         :string(1)        not null
+#  description :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
+class Cat < ApplicationRecord
+  COLORS = ["brown", "black", "white", "snickers"].freeze
+  validates :sex, inclusion: { in: ["M", "F"] }
+  validates :color, inclusion: { in: COLORS }
+  validates :birth_date, :color, :name, :sex, presence: true 
+  include ActionView::Helpers::DateHelper
+
+  def age
+    distance_of_time_in_words(birth_date, Time.now)
+  end
+
+  def colors
+    COLORS
+  end
+
+end
---
+++ ninety_nine_cats/db/migrate/20181218170905_add_sex_limit_to_cats.rb
@@ -0,0 +1,5 @@
+class AddSexLimitToCats < ActiveRecord::Migration[5.2]
+  def change
+    change_column :cats, :sex, :string, limit: 1
+  end
+end
